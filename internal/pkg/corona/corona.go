21403c69df5ad1191bed1b40cdc4feff5ea18b5f
Anton Andersson <anton.andersson@protegrity.com>
2020-04-01 11:32:32 +0200

    Fix corona API updates

--- internal/pkg/corona/corona.go
+++ internal/pkg/corona/corona.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/patrickmn/go-cache"
@@ -13,32 +14,51 @@ import (
 	"github.com/antonjah/leif/internal/pkg/constants"
 )
 
-type countryStatus struct {
-	Country      string `json:"country"`
-	Province     string `json:"province"`
-	Confirmed    int    `json:"confirmed"`
-	Recovered    int    `json:"recovered"`
-	Deaths       int    `json:"deaths"`
-	MortalityPer string `json:"mortalityPer"`
-	RecoveredPer string `json:"recoveredPer"`
-	LastUpdated  string `json:"lastUpdated"`
+type CoronaResponse struct {
+	Data []CountryInfo `json:"data"`
 }
 
-func (s *countryStatus) toString() string {
-	if s.Province != "" {
-		return fmt.Sprintf(
-			"Province: %s\nConfirmed: %d\nRecovered: %d\nDeaths: %d\nMortality Rate: %s%%\nRecover Rate: %s%%\nLast Updated: %s\n\n",
-			s.Province, s.Confirmed, s.Recovered, s.Deaths, s.MortalityPer, s.RecoveredPer, s.LastUpdated,
-		)
-	} else {
-		return fmt.Sprintf(
-			"Confirmed: %d\nRecovered: %d\nDeaths: %d\nMortality Rate: %s%%\nRecover Rate: %s%%\nLast Updated: %s\n\n",
-			s.Confirmed, s.Recovered, s.Deaths, s.MortalityPer, s.RecoveredPer, s.LastUpdated,
-		)
-	}
+type CountryInfo struct {
+	Country             string `json:"country"`
+	Cases               int    `json:"cases"`
+	TodayCases          int    `json:"todayCases"`
+	Deaths              int    `json:"deaths"`
+	TodayDeaths         int    `json:"todayDeaths"`
+	Recovered           int    `json:"recovered"`
+	Active              int    `json:"active"`
+	Critical            int    `json:"critical"`
+	CasesPerOneMillion  int    `json:"casesPerOneMillion"`
+	DeathsPerOneMillion int    `json:"deathsPerOneMillion"`
+	Confirmed           int    `json:"confirmed"`
+}
+
+func (c *CoronaResponse) toString() string {
+	return fmt.Sprintf(
+		"Confirmed: %d\n"+
+			"Cases: %d\n"+
+			"Cases today: %d\n"+
+			"Deaths: %d\n"+
+			"Deaths today: %d\n"+
+			"Recovered: %d\n"+
+			"Active: %d\n"+
+			"Critical: %d\n"+
+			"Cases per million: %d\n"+
+			"Deaths per million: %d",
+		c.Data[0].Confirmed,
+		c.Data[0].Cases,
+		c.Data[0].TodayCases,
+		c.Data[0].Deaths,
+		c.Data[0].TodayDeaths,
+		c.Data[0].Recovered,
+		c.Data[0].Active,
+		c.Data[0].Critical,
+		c.Data[0].CasesPerOneMillion,
+		c.Data[0].DeathsPerOneMillion,
+	)
 }
 
 func GetStatuses(country string, cache cache.Cache, logger *logrus.Logger) string {
+	country = strings.ToLower(country)
 	cachedCountry, found := cache.Get(country)
 	if found {
 		cached := cachedCountry.(string)
@@ -50,6 +70,11 @@ func GetStatuses(country string, cache cache.Cache, logger *logrus.Logger) strin
 	if err != nil {
 		logger.Error(err)
 		return "Failed to get corona status, please check my logs"
+	} else if r.StatusCode == http.StatusNotFound {
+		return "Country not found"
+	} else if r.StatusCode / 100 != 2 {
+		logger.Error(r.Status)
+		return "Failed to get corona status, please check my logs"
 	}
 
 	b, err := ioutil.ReadAll(r.Body)
@@ -59,18 +84,13 @@ func GetStatuses(country string, cache cache.Cache, logger *logrus.Logger) strin
 	}
 	defer r.Body.Close()
 
-	var countryStatuses []countryStatus
-	if err := json.Unmarshal(b, &countryStatuses); err != nil {
-		logger.Error(err)
-		return "Country not found"
+	var res CoronaResponse
+	if err := json.Unmarshal(b, &res); err != nil {
+		logger.Fatal(err)
+		return "Failed to read corona status, please check my logs"
 	}
 
-	var cs string
-	for _, s := range countryStatuses {
-		cs = cs + s.toString()
-	}
+	cache.Set(country, res.toString(), 24*time.Hour)
 
-	cache.Set(country, cs, 24*time.Hour)
-
-	return cs
+	return res.toString()
 }
